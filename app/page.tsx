35dc226e3b773afbbeb51584dd94cc491c6ca514
lilcroissant5 <ecmayaa@icloud.com>
2026-01-25 16:10:58 -0700

    removed button temporarily

--- app/page.tsx
+++ app/page.tsx
@@ -31,35 +31,12 @@ export default function Home() {
               ESPECIALISTAS EN <br />NARANJA PARA JUGO
             </h1>
           </div>
-          <div
-            className={`absolute bottom-10  left-1/2 -translate-x-1/2 w-[90%] max-w-xl
-                        flex flex-col items-center justify-center mt-16
-                        `}
-          >
-            <h1
-              className={`text-green text-center drop-shadow-lg
-                        text-[clamp(1rem,4vw,4rem)] leading-tight ${libreBodoni.className}`}
-            >
+          <div className={`absolute bottom-[clamp(1rem,0vw,10rem)] left-1/2 -translate-x-1/2 w-[90%] max-w-xl flex flex-col items-center justify-center mt-16`}>
+            <h1 className={`text-green text-center drop-shadow-lg text-[clamp(1rem,5vw,4rem)] leading-tight ${libreBodoni.className}`}>
               Los más frescos cítricos y frutas para restaurantes y mayoristas en Mercado Gómez Campana.
             </h1>
-              <Link
-                        href="https://wa.me/6671270782?text=Hola%20quiero%20información%20sobre%20sus%20productos"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="mt-6 group relative inline-flex items-center justify-center gap-3 px-15 py-1 bg-gradient-to-r from-orange-500 to-orange-600 text-white font-medium rounded-full border border-white/20 shadow-lg shadow-orange-500/30 transition-all duration-300 hover:shadow-orange-500/50 hover:-translate-y-0.5 active:scale-95 overflow-hidden"
-                    >
-                        {/* Shimmer effect on hover */}
-                        <div className="absolute inset-0 w-full h-full bg-gradient-to-r from-transparent via-white/10 to-transparent -translate-x-full group-hover:animate-[shimmer_1.5s_infinite]" />
 
-                        <span className="relative flex flex-col items-center leading-tight">
-                            <span className="text-sm font-bold tracking-wide">WhatsApp</span>
-                            <span className="text-[10px] uppercase tracking-[0.1em] opacity-90 font-black">
-                                ¡ORDENA AQUI!
-                            </span>
-                        </span>
-                    </Link>
           </div>
-          
         </div>
 
         {/**************************************************************************  Desktop / large screen */}
